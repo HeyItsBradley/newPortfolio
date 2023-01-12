3185346b24f77ab8a1f153cfb0b9f7c156ec99d5
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-01-12 16:18:53 -0700

    added a pfp to the about me page

--- my-app/src/components/AboutMe.js
+++ my-app/src/components/AboutMe.js
@@ -1,16 +1,20 @@
 import React from "react";
+import pfp from "./profileImage.jpg";
 
 function AboutMe() {
   return (
     <div className="container-fluid">
-      <div className="row text-light text-center">
-        <h2 className="col-lg-6 mt-5 px-5">
-          Hi ! I'm Bradley, I am a full stack Developer lookings for
-          opportunities in the tech industry. I have completed a Full Stack Web
-          Development Certificate program at Denver Universtiy. I have learned
-          many popular technologies and am always looking for new ways to
-          improve my skill set.
-        </h2>
+      <div className="row text-light text-center mt-5">
+        <div className="col-lg-6 px-5">
+          <img className="col-lg-2 mt-5" src={pfp} alt="Profile picture"></img>
+          <h2 className=" mt-5 px-5">
+            Hi ! I'm Bradley, I am a full stack Developer lookings for
+            opportunities in the tech industry. I have completed a Full Stack
+            Web Development Certificate program at Denver Universtiy. I have
+            learned many popular technologies and am always looking for new ways
+            to improve my skill set.
+          </h2>
+        </div>
         <h2 className="col-lg-6 mt-5 mb-5">
           Familiar with:
           <li className="col-lg-12 mt-4">JavaScript</li>
