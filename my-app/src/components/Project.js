3593740ca3b4712d0b4e9000bc0ae960de211b2a
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-01-12 18:39:08 -0700

    added the rest of the projects

--- my-app/src/components/Project.js
+++ my-app/src/components/Project.js
@@ -1,14 +1,29 @@
 import React from "react";
 import DevDive from "./devDive.png";
 import ParkFinder from "./parkFinder.png";
+import jate from "./JATE.png";
+import val from "./val.png";
+import weather from "./weather.png";
+import password from "./passwordGen.png";
 
 function Project() {
   return (
-    <div className="container-fluid pt-5">
+    <div className="container-fluid pt-5 pb-5">
       <div className="justify-content-around row pt-5">
         <div className="col-3">
           <div className="card col-12">
-            <img src={DevDive} className="card-img-top" alt="..."></img>
+            <a
+              href="https://dev-dive.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={DevDive}
+                className="card-img-top project"
+                alt="..."
+                href="https://dev-dive.herokuapp.com/"
+              ></img>
+            </a>
             <div class="card-body">
               <p class="card-text font-link">
                 DevDive, a concept for a developer job finding platform.
@@ -29,7 +44,48 @@ function Project() {
         </div>
         <div className="col-3">
           <div className="card col-12">
-            <img src={ParkFinder} className="card-img-top" alt="..."></img>
+            <a
+              href="https://bradleys-text-editor.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={jate}
+                className="card-img-top project"
+                alt="..."
+                href="https://bradleys-text-editor.herokuapp.com/"
+              ></img>
+            </a>
+            <div class="card-body">
+              <p class="card-text font-link">
+                JATE (Just, another text editor. A simple text editing app for
+                writing code snippits. Is a PWA and can be downloaded and ran
+                offline)
+              </p>
+              <a
+                href="https://github.com/HeyItsBradley/Text-Editor"
+                class="card-link font-link text-black"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card col-12">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://heyitsbradley.github.io/National-Park-Finder-with-Directions/"
+            >
+              <img
+                src={ParkFinder}
+                className="card-img-top project"
+                alt="..."
+              ></img>
+            </a>
             <div class="card-body">
               <p class="card-text font-link">
                 National Park finder, this app allows you to select a state,
@@ -40,6 +96,97 @@ function Project() {
               </p>
               <a
                 href="https://github.com/HeyItsBradley/National-Park-Finder-with-Directions"
+                class="card-link font-link text-black"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github Repo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="justify-content-around row pb-5 pt-5">
+        <div className="col-3">
+          <div className="card col-12">
+            <a
+              href="https://heyitsbradley.github.io/Valorant-Agents/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={val}
+                className="card-img-top project"
+                alt="..."
+                href="https://heyitsbradley.github.io/Valorant-Agents/"
+              ></img>
+            </a>
+            <div class="card-body">
+              <p class="card-text font-link">
+                Valorant agents, a project using an api to bring in assets of
+                the characters from the popular video game valorant.
+              </p>
+              <a
+                href="https://github.com/HeyItsBradley/Valorant-Agents"
+                class="card-link font-link text-black"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card col-12">
+            <a
+              href="https://heyitsbradley.github.io/Weather-Dashboard/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={weather}
+                className="card-img-top project"
+                alt="..."
+                href="https://heyitsbradley.github.io/Weather-Dashboard/"
+              ></img>
+            </a>
+            <div class="card-body">
+              <p class="card-text font-link">
+                Weather Dashboard, display the weather for a selected city, uses
+                openWeatherApi
+              </p>
+              <a
+                href="https://github.com/HeyItsBradley/Weather-Dashboard"
+                class="card-link font-link text-black"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card col-12">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://heyitsbradley.github.io/RandomPasswordGenerator/"
+            >
+              <img
+                src={password}
+                className="card-img-top project"
+                alt="password generator"
+              ></img>
+            </a>
+            <div class="card-body">
+              <p class="card-text font-link">
+                Password generator, creates a reandomized password based on the
+                filters selected by the user.
+              </p>
+              <a
+                href="https://github.com/HeyItsBradley/RandomPasswordGenerator"
                 class="card-link font-link text-black"
                 target="_blank"
                 rel="noreferrer"
